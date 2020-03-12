bc568df18e8a8261d0b52f553a8ae4a875490ce3
Yamil Gonzales <yamilg@gmail.com>
2020-03-11 19:11:47 -0600

    change server response

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 app.set('port', process.env.PORT || 5000);
 
 app.get('/', function(req, res) {
-  res.send('Hello Node + Github!');
+  res.send('Hello Node + Github! automatic deployments are working!');
 });
 
 app.listen(app.get('port'), function() {
