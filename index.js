9f11c7ab8d8e5cf36d875f71cdb060060dc46468
Yamil Gonzales <yamil@protonmail.com>
2020-06-05 16:48:14 -0600

    change the syntax of the import

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var express = require('express');
+import express from "express";
 var app = express();
-app.set('port', process.env.PORT || 5000);
+app.set("port", process.env.PORT || 5000);
 
-app.get('/', function(req, res) {
-  res.send('Hello Node + Github! automatic deployments are working!');
+app.get("/", function (req, res) {
+  res.send("Hello Node + Github! automatic deployments are working!");
 });
 
-app.listen(app.get('port'), function() {
-  console.log('App is listening on port ' + app.get('port'));
+app.listen(app.get("port"), function () {
+  console.log("App is listening on port " + app.get("port"));
 });
